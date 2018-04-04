ecbe448b3fa09aca7a4ce68b12585012480917b7
fabin <fabin@DESKTOP-RIU5EIA>
2018-04-04 10:14:04 +0200

    COnfirmado etapa

---
+++ CalculadoraFabini/fMain.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace CalculadoraFabini
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+            this.btnNumber9 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnNumber8 = new System.Windows.Forms.Button();
+            this.btnNumber7 = new System.Windows.Forms.Button();
+            this.btnNumber6 = new System.Windows.Forms.Button();
+            this.btnNumber5 = new System.Windows.Forms.Button();
+            this.btnNumber4 = new System.Windows.Forms.Button();
+            this.btnNumber3 = new System.Windows.Forms.Button();
+            this.btnNumber2 = new System.Windows.Forms.Button();
+            this.btnNumber1 = new System.Windows.Forms.Button();
+            this.btnNumber0 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnNumber9
+            // 
+            resources.ApplyResources(this.btnNumber9, "btnNumber9");
+            this.btnNumber9.Name = "btnNumber9";
+            this.btnNumber9.UseVisualStyleBackColor = true;
+            this.btnNumber9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            resources.ApplyResources(this.archivoToolStripMenuItem, "archivoToolStripMenuItem");
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            // 
+            // salirToolStripMenuItem
+            // 
+            resources.ApplyResources(this.salirToolStripMenuItem, "salirToolStripMenuItem");
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            // 
+            // btnNumber8
+            // 
+            resources.ApplyResources(this.btnNumber8, "btnNumber8");
+            this.btnNumber8.Name = "btnNumber8";
+            this.btnNumber8.UseVisualStyleBackColor = true;
+            this.btnNumber8.Click += new System.EventHandler(this.btnNumber8_Click);
+            // 
+            // btnNumber7
+            // 
+            resources.ApplyResources(this.btnNumber7, "btnNumber7");
+            this.btnNumber7.Name = "btnNumber7";
+            this.btnNumber7.UseVisualStyleBackColor = true;
+            this.btnNumber7.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnNumber6
+            // 
+            resources.ApplyResources(this.btnNumber6, "btnNumber6");
+            this.btnNumber6.Name = "btnNumber6";
+            this.btnNumber6.UseVisualStyleBackColor = true;
+            this.btnNumber6.Click += new System.EventHandler(this.btnNumber6_Click);
+            // 
+            // btnNumber5
+            // 
+            resources.ApplyResources(this.btnNumber5, "btnNumber5");
+            this.btnNumber5.Name = "btnNumber5";
+            this.btnNumber5.UseVisualStyleBackColor = true;
+            this.btnNumber5.Click += new System.EventHandler(this.btnNumber5_Click);
+            // 
+            // btnNumber4
+            // 
+            resources.ApplyResources(this.btnNumber4, "btnNumber4");
+            this.btnNumber4.Name = "btnNumber4";
+            this.btnNumber4.UseVisualStyleBackColor = true;
+            this.btnNumber4.Click += new System.EventHandler(this.btnNumber4_Click);
+            // 
+            // btnNumber3
+            // 
+            resources.ApplyResources(this.btnNumber3, "btnNumber3");
+            this.btnNumber3.Name = "btnNumber3";
+            this.btnNumber3.UseVisualStyleBackColor = true;
+            this.btnNumber3.Click += new System.EventHandler(this.btnNumber3_Click);
+            // 
+            // btnNumber2
+            // 
+            resources.ApplyResources(this.btnNumber2, "btnNumber2");
+            this.btnNumber2.Name = "btnNumber2";
+            this.btnNumber2.UseVisualStyleBackColor = true;
+            this.btnNumber2.Click += new System.EventHandler(this.btnNumber2_Click);
+            // 
+            // btnNumber1
+            // 
+            resources.ApplyResources(this.btnNumber1, "btnNumber1");
+            this.btnNumber1.Name = "btnNumber1";
+            this.btnNumber1.UseVisualStyleBackColor = true;
+            this.btnNumber1.Click += new System.EventHandler(this.btnNumber1_Click);
+            // 
+            // btnNumber0
+            // 
+            resources.ApplyResources(this.btnNumber0, "btnNumber0");
+            this.btnNumber0.Name = "btnNumber0";
+            this.btnNumber0.UseVisualStyleBackColor = true;
+            this.btnNumber0.Click += new System.EventHandler(this.y);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // fMain
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnNumber0);
+            this.Controls.Add(this.btnNumber1);
+            this.Controls.Add(this.btnNumber2);
+            this.Controls.Add(this.btnNumber3);
+            this.Controls.Add(this.btnNumber4);
+            this.Controls.Add(this.btnNumber5);
+            this.Controls.Add(this.btnNumber6);
+            this.Controls.Add(this.btnNumber7);
+            this.Controls.Add(this.btnNumber8);
+            this.Controls.Add(this.btnNumber9);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "fMain";
+            this.Load += new System.EventHandler(this.fMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNumber9;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.Button btnNumber8;
+        private System.Windows.Forms.Button btnNumber7;
+        private System.Windows.Forms.Button btnNumber6;
+        private System.Windows.Forms.Button btnNumber5;
+        private System.Windows.Forms.Button btnNumber4;
+        private System.Windows.Forms.Button btnNumber3;
+        private System.Windows.Forms.Button btnNumber2;
+        private System.Windows.Forms.Button btnNumber1;
+        private System.Windows.Forms.Button btnNumber0;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CalculadoraFabini/fMain.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraFabini
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNumber8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNumber6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNumber5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNumber4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNumber3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNumber2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNumber1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void y(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
